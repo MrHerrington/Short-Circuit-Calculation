2c76250c8803b07fdb48a3c8b4da828bbc411536
Ilya Belov <154321709+MrHerrington@users.noreply.github.com>
2024-03-26 16:17:28 +0300

    Testing filling table

--- create_electrical_product_catalog.py
+++ create_electrical_product_catalog.py
@@ -1,64 +1,66 @@
-from sqlalchemy import Column, String, Integer, Numeric, ForeignKey
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import relationship
+import json
+import sqlalchemy as sa
+import sqlalchemy.orm
 
 
-def db_access():
-    with open('DB_security.txt', 'r', encoding='UTF-8') as file:
-        login, password, db_name = file.read().splitlines()
-    return f"mysql+pymysql://{login}:{password}@localhost/{db_name}?charset=utf8mb4"
+def db_access() -> str:
+
+    def keys_getter() -> str:
+        with open('db_security.json', 'r', encoding='UTF-8') as file:
+            temp = json.load(file)['db_access']
+            login, password, db_name = temp['login'], temp['password'], temp['db_name']
+            db_access.engine_string = f"mysql+pymysql://{login}:{password}@localhost/{db_name}?charset=utf8mb4"
+        return db_access.engine_string
+
+    try:
+        return db_access.engine_string
+    except AttributeError:
+        keys_getter()
+        return db_access.engine_string
 
 
-class Base(DeclarativeBase):
+class Base(sa.orm.DeclarativeBase):
     pass
-
-
-# # Типы продукции: трансформатор, кабель, автомат, рубильник, etc
-# class Product(Base):
-#     __tablename__ = 'product'
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     product_type = Column(String)
 
 
 # Субтаблицы с параметрами трансформаторов
 # Таблица номинальных мощностей трансформаторов
 class PowerNominal(Base):
     __tablename__ = 'power_nominal'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    power_nominal = Column(Integer, nullable=False)
-    transformers = relationship('transformers', back_populates='power_nominals')
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    power = sa.Column(sa.Integer, nullable=False)
+    transformers = sa.orm.relationship('Transformer', back_populates='power_nominals')
 
 
 # Таблица номинальных напряжений трансформаторов
 class VoltageNominal(Base):
     __tablename__ = 'voltage_nominal'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    voltage_nominal = Column(Numeric(1, 1), nullable=False)
-    transformers = relationship('transformers', back_populates='voltage_nominals')
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    voltage = sa.Column(sa.Numeric(1, 1), nullable=False)
+    transformers = sa.orm.relationship('Transformer', back_populates='voltage_nominals')
 
 
 # Таблица групп соединений обмоток трансформаторов
 class VectorGroup(Base):
     __tablename__ = 'vector_group'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    vector_group = Column(String(10))
-    transformers = relationship('transformers', back_populates='vector_groups')
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    vector_gr = sa.Column(sa.String(10))
+    transformers = sa.orm.relationship('Transformer', back_populates='vector_groups')
 
 
 # Таблица связи по трансформаторам с мощностью потерь и напряжением короткого замыкания
 class Transformer(Base):
     __tablename__ = 'transformer'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    Snom_id = Column(Integer, ForeignKey(PowerNominal.id, ondelete='CASCADE'))
-    Unom_id = Column(Integer, ForeignKey(VoltageNominal.id, ondelete='CASCADE'))
-    vector_group_id = Column(Integer, ForeignKey(VectorGroup.id, ondelete='CASCADE'))
-    power_short_circuit = Column(Numeric(2, 2), nullable=False)
-    voltage_short_circuit = Column(Numeric(1, 1), nullable=False)
-    power_nominals = relationship(PowerNominal.__tablename__, back_populates='transformers')
-    voltage_nominals = relationship(VoltageNominal.__tablename__, back_populates='transformers')
-    vector_groups = relationship(VectorGroup.__tablename__, back_populates='transformers')
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    Snom_id = sa.Column(sa.Integer, sa.ForeignKey(PowerNominal.id, ondelete='CASCADE'))
+    Unom_id = sa.Column(sa.Integer, sa.ForeignKey(VoltageNominal.id, ondelete='CASCADE'))
+    vector_group_id = sa.Column(sa.Integer, sa.ForeignKey(VectorGroup.id, ondelete='CASCADE'))
+    power_short_circuit = sa.Column(sa.Numeric(2, 2), nullable=False)
+    voltage_short_circuit = sa.Column(sa.Numeric(1, 1), nullable=False)
+    power_nominals = sa.orm.relationship('PowerNominal', back_populates='transformers')
+    voltage_nominals = sa.orm.relationship('VoltageNominal', back_populates='transformers')
+    vector_groups = sa.orm.relationship('VectorGroup', back_populates='transformers')
 
 
-engine = create_engine(db_access(), echo=True)
+engine = sa.create_engine(db_access(), echo=True)
 Base.metadata.create_all(engine)
---
+++ insert_teble_test.py
@@ -0,0 +1,11 @@
+import sqlalchemy as sa
+import sqlalchemy.orm
+from create_electrical_product_catalog import engine, PowerNominal, VoltageNominal, VectorGroup, Transformer
+
+Session = sa.orm.sessionmaker(engine)
+
+with Session() as session:
+    for i in [25, 40, 63, 100, 160, 250, 400, 630, 1000, 1600, 2000]:
+        if i not in list(map(lambda x: x.power, session.query(PowerNominal))):
+            session.execute(sa.insert(PowerNominal), [{'power': i}])
+    session.commit()
