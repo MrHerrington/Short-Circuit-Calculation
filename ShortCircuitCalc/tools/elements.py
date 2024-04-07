010d0d31103cc876f3f741c2f6b37c5a8a9660cc
Ilya Belov <154321709+MrHerrington@users.noreply.github.com>
2024-04-07 22:31:12 +0300

    Project architecture of elements.

--- ShortCircuitCalc/tools/elements.py
+++ ShortCircuitCalc/tools/elements.py
@@ -1,6 +1,36 @@
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+# from decimal import Decimal
+# from ShortCircuitCalc.tools import session_scope
+# from ShortCircuitCalc.database.transformer import *
 
 
-class FullResistance():
-    def __get__(self, instance, owner):
-        pass
+@dataclass
+class T:
+    __slots__ = ('power', 'voltage', 'vector_group')
+    power: int
+    voltage: float
+    vector_group: str
+
+
+@dataclass
+class Q:
+    __slots__ = ('device_type', 'current_value')
+    current_value: int
+    device_type: str
+
+
+@dataclass
+class QF(Q):
+    device_type: str = field(default='Автомат')
+
+
+@dataclass
+class QS(Q):
+    device_type: str = field(default='Рубильник')
+
+
+# with session_scope() as session:
+#     pass
+
+print(QS(25))
