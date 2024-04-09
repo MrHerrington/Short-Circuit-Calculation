1e8b48c71d635a8527b6c42fd8ba9ede9efe5a32
Ilya Belov <154321709+MrHerrington@users.noreply.github.com>
2024-04-09 18:11:05 +0300

    Update elements.py, config, complete dataclasses, write methods / properties for new class Calculator, new positions in ToDo list, install sphinx, testing formula string mapping

--- ShortCircuitCalc/config.py
+++ ShortCircuitCalc/config.py
@@ -2,10 +2,16 @@
 
 
 from pathlib import Path
+from decimal import Decimal
 
 
+# Program settings
 ROOT_DIR = Path(__file__).resolve().parent
 DB_SECURITY_DIR = ROOT_DIR / 'db_security.json'
 DATA_DIR = ROOT_DIR / 'data'
 ENGINE_ECHO = False
 DB_TABLES_CLEAR_INSTALL = True
+
+# Calculations settings
+SYSTEM_VOLTAGE_IN_KILOVOLTS = Decimal('0.4')
+CALCULATIONS_ACCURACY = 3
--- ShortCircuitCalc/tools/elements.py
+++ ShortCircuitCalc/tools/elements.py
@@ -1,11 +1,14 @@
 """This module contains classes for various electrical elements such as transformers, cables
 and contacts, for programmatically inputting data related to these categories."""
 
-
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from decimal import Decimal
+import typing as ty
+import math
+from functools import reduce
 from ShortCircuitCalc.tools import session_scope
+from ShortCircuitCalc.config import SYSTEM_VOLTAGE_IN_KILOVOLTS, CALCULATIONS_ACCURACY
 from ShortCircuitCalc.database.transformer import *
 from ShortCircuitCalc.database.cable import *
 from ShortCircuitCalc.database.contact import *
@@ -15,40 +18,40 @@ class BaseElement(ABC):
     """The abstract class for all elements"""
     __slots__ = ()
 
+    @property
     def resistance_r1(self) -> Decimal:
         """The method searches in the database resistance R1."""
-        query_val = self.sql_query('resistance_r1')
-        if query_val:
-            return query_val
-        else:
+        query_val = self._sql_query('resistance_r1')
+        if query_val is None:
             raise ValueError('The resistance R1 is not found in the database!')
+        return query_val
 
+    @property
     def reactance_x1(self) -> Decimal:
         """The method searches in the database reactance X1."""
-        query_val = self.sql_query('reactance_x1')
-        if query_val:
-            return query_val
-        else:
+        query_val = self._sql_query('reactance_x1')
+        if query_val is None:
             raise ValueError('The reactance X1 is not found in the database!')
+        return query_val
 
+    @property
     def resistance_r0(self) -> Decimal:
         """The method searches in the database resistance R0."""
-        query_val = self.sql_query('resistance_r0')
-        if query_val:
-            return query_val
-        else:
+        query_val = self._sql_query('resistance_r0')
+        if query_val is None:
             raise ValueError('The resistance R0 is not found in the database!')
+        return query_val
 
+    @property
     def reactance_x0(self) -> Decimal:
         """The method searches in the database reactance X0."""
-        query_val = self.sql_query('reactance_x0')
-        if query_val:
-            return query_val
-        else:
+        query_val = self._sql_query('reactance_x0')
+        if query_val is None:
             raise ValueError('The reactance X0 is not found in the database!')
+        return query_val
 
     @abstractmethod
-    def sql_query(self, attr_name) -> 'sa.scalar':
+    def _sql_query(self, attr_name) -> 'sa.scalar':
         """Returns the resistance value from the database.
 
         Args:
@@ -63,20 +66,20 @@ class BaseElement(ABC):
 @dataclass
 class T(BaseElement):
     """The class for transformer"""
-    __slots__ = ('power', 'voltage', 'vector_group')
+    __slots__ = ('power', 'vector_group')
     power: int
-    voltage: float
     vector_group: str
+    voltage: Decimal = field(init=False, default=SYSTEM_VOLTAGE_IN_KILOVOLTS)
 
-    def sql_query(self, attr_name) -> 'sa.scalar':
+    def _sql_query(self, attr_name) -> 'sa.scalar':
         with session_scope() as session:
             return session.execute(sa.select(getattr(Transformer, attr_name)).
                                    join(PowerNominal, Transformer.power_id == PowerNominal.id).
                                    join(VoltageNominal, Transformer.voltage_id == VoltageNominal.id).
                                    join(Scheme, Transformer.vector_group_id == Scheme.id).
                                    where(sa.and_(PowerNominal.power == self.power,
-                                                 VoltageNominal.voltage == Decimal(str(self.voltage)),
-                                                 Scheme.vector_group == self.vector_group))).scalar()
+                                                 VoltageNominal.voltage == self.voltage),
+                                         Scheme.vector_group == self.vector_group)).scalar()
 
 
 @dataclass
@@ -86,10 +89,10 @@ class W(BaseElement):
     mark: str
     amount: int
     range_val: float
-    # length in meters
+    # Length in meters
     length: int
 
-    def sql_query(self, attr_name) -> 'sa.scalar':
+    def _sql_query(self, attr_name) -> 'sa.scalar':
         """Returns the resistance value for the specified length."""
         with session_scope() as session:
             query_val = session.execute(sa.select(getattr(Cable, attr_name)).
@@ -108,7 +111,7 @@ class Q(BaseElement):
     current_value: int
     device_type: str
 
-    def sql_query(self, attr_name) -> 'sa.scalar':
+    def _sql_query(self, attr_name) -> 'sa.scalar':
         with session_scope() as session:
             return session.execute(sa.select(getattr(CurrentBreaker, attr_name)).
                                    join(Device, CurrentBreaker.device_type_id == Device.id).
@@ -132,28 +135,91 @@ class R(BaseElement):
     __slots__ = 'contact_type'
     contact_type: str
 
-    def sql_query(self, attr_name) -> 'sa.scalar':
+    def _sql_query(self, attr_name) -> 'sa.scalar':
         with session_scope() as session:
             return session.execute(sa.select(getattr(OtherContact, attr_name)).
                                    where(OtherContact.contact_type == self.contact_type)).scalar()
 
 
-print(T(25, 0.4, 'У/Ун-0').resistance_r1())
-print(T(25, 0.4, 'У/Ун-0').reactance_x1())
-print(T(25, 0.4, 'У/Ун-0').resistance_r0())
-print(T(25, 0.4, 'У/Ун-0').reactance_x0())
-print()
+class Calculator:
+    """The class for short circuit calculations"""
 
-print(W('СИП', 3, 120, 1000).resistance_r1())
-print(W('СИП', 3, 120, 1000).reactance_x1())
-print(W('СИП', 3, 120, 1000).resistance_r0())
-print(W('СИП', 3, 120, 1000).reactance_x0())
-print()
+    def __init__(self, obj: ty.Sequence[BaseElement]) -> None:
+        self._obj = obj
 
-print(Q(4, 'Автомат').resistance_r1())
-print(QF(4).resistance_r1())
-print(QS(4).resistance_r1())
-print()
+    @property
+    def obj(self) -> ty.Sequence[BaseElement]:
+        return self._obj
 
-print(R('Дуга').resistance_r1())
-print(R('РУ').resistance_r1())
+    @property
+    def three_phase_current_short_circuit(self) -> Decimal:
+        """Calculates the three-phase current during a short circuit.
+
+        Returns:
+            Decimal: The calculated three-phase current during a short circuit.
+        .. math::
+            I_{k^{(3)}} = \\frac{U}{\\sqrt{3} * z_{^{(3)}}}
+        :math:`U` - voltage value,
+        :math:`z_{^{(3)}}` - three-phase summary resistance.
+
+        """
+        return round(
+            SYSTEM_VOLTAGE_IN_KILOVOLTS / Decimal(math.sqrt(3)) / self.__three_phase_summary_resistance(),
+            CALCULATIONS_ACCURACY)
+
+    @property
+    def two_phase_voltage_short_circuit(self) -> Decimal:
+        """Calculates the two-phase current during a short circuit.
+
+        Returns:
+            Decimal: The calculated two-phase current during a short circuit.
+        .. math::
+            I_{k^{(2)}} = \\frac{\\sqrt{3} * I_{k^{(3)}}}{2}
+        :math:`I_{k^{(3)}}` - three-phase current during a short circuit.
+
+        """
+        return round(
+            Decimal(math.sqrt(3)) / 2 * self.three_phase_current_short_circuit,
+            CALCULATIONS_ACCURACY)
+
+    def __three_phase_summary_resistance(self) -> Decimal:
+        """
+        Service function, calculates three-phase summary resistance.
+
+        Returns:
+            single Decimal value.
+        .. math::
+            z_{^{(3)}} = \\sqrt{r_{1\\sum_{}^{2}} + x_{1\\sum_{}^{2}}}
+        :math:`r_1` - resistance value resistance_r1,
+        :math:`x_1` - reactance value reactance_x1.
+
+        """
+        return Decimal(math.sqrt(math.pow(
+            reduce(lambda x, y: x + y, (
+                i.resistance_r1 for i in self.obj)), 2) + math.pow(
+            reduce(lambda x, y: x + y, (
+                i.reactance_x1 for i in self.obj)), 2)))
+
+
+# print(T(25, 'У/Ун-0').resistance_r1)
+# print(T(25, 'У/Ун-0').reactance_x1)
+# print(T(25, 'У/Ун-0').resistance_r0)
+# print(T(25, 'У/Ун-0').reactance_x0)
+# print()
+#
+# print(W('СИП', 3, 120, 1000).resistance_r1)
+# print(W('СИП', 3, 120, 1000).reactance_x1)
+# print(W('СИП', 3, 120, 1000).resistance_r0)
+# print(W('СИП', 3, 120, 1000).reactance_x0)
+# print()
+#
+# print(Q(4, 'Автомат').resistance_r1)
+# print(QF(4).resistance_r1)
+# print(QS(4).resistance_r1)
+# print()
+#
+# print(R('РУ').resistance_r1)
+D = R('Дуга')
+TCH = T(160, 'У/Ун-0')
+print(Calculator((TCH, D)).three_phase_current_short_circuit)
+print(Calculator((TCH, D)).two_phase_voltage_short_circuit)
