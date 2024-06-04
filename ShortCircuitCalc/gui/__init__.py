04db98c3d6eea13c80e7cafd5f6171a876f94d8e
Ilya Belov <154321709+MrHerrington@users.noreply.github.com>
2024-06-04 13:17:14 +0300

    test rename

--- ShortCircuitCalc/gui/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""This package provides tools for getting information from program through GUI."""
-
-
-from shortcircuitcalc.gui.windows import *
-from shortcircuitcalc.gui.figures import *
--- ShortCircuitCalc/tools/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""This package provides tools for interacting with catalog databases and utility
-tools for the main functionality of the program. Also, the package contains
-classes for various electrical elements such as transformers, cables and
-contacts, for programmatically inputting data related to these categories."""
-
-
-from shortcircuitcalc.tools.tools import *
-from shortcircuitcalc.tools.elements import *
